4034ca522a505c1f295500f5cff350859ca72900
Noah018dev <140430451+Noah018dev@users.noreply.github.com>
2025-01-29 19:44:07 -0800

    its version 0.0.5 now!!!
    update version number and publish to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='multithreaded',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages(),
     install_requires=['cloudpickle', 'logzero', 'stop-thread'],
     author='Noah018',
